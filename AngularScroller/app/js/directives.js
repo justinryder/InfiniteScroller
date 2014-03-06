1c12e72209a4e66b8b11176f311c20bcc3c00d40
Colin K-R <cpk6503@gmail.com>
2014-03-06 16:04:25 -0500

    Added scroll-wheel catching to the game

--- AngularScroller/app/js/directives.js
+++ AngularScroller/app/js/directives.js
@@ -11,31 +11,16 @@ angular.module('myApp.directives', []).
   }]);
 
 angular.module('myApp.directives', []).
-  directive('.game', function() {
+  directive('body', function() {
   return {
 		restrict: 'EA',
-		replace: 'true',
 		transclude: 'true',
-		template: 
-		  '<div><ul class="clist" ng-transclude></ul>' + 
-	    '<a href="" class="clist-up" ng-click="up()">Up</a>' + 
-	    '<a href="" class="clist-down" ng-click="down()">Down</a></div>',
+		template: '<div><ul class="clist" ng-transclude></ul>',
 		link: function (scope, element, attrs) {
-			scope.up = function () {
-				alert("UP!");
-				console.log("Up");
-			}
-
-			scope.down = function () {
-				console.log("Down");
-			}
             Hamster(element[0]).wheel(function(event, delta, deltaX, deltaY){
                 event.preventDefault();
-              if(delta>0)
-                  scope.down();
-              else
-                  scope.up();
+                console.log("Scroll");
             });
 		}
-	}
+	};
 });
