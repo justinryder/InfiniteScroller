30e0a45a2a32dd05c1f4d0fd3e724e9d99e11a85
Colin K-R <cpk6503@gmail.com>
2014-03-06 15:22:22 -0500

    Added first version of scroll wheel support

--- AngularScroller/app/js/directives.js
+++ AngularScroller/app/js/directives.js
@@ -9,3 +9,33 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   }]);
+
+angular.module('myApp.directives', []).
+  directive('.game', function() {
+  return {
+		restrict: 'EA',
+		replace: 'true',
+		transclude: 'true',
+		template: 
+		  '<div><ul class="clist" ng-transclude></ul>' + 
+	    '<a href="" class="clist-up" ng-click="up()">Up</a>' + 
+	    '<a href="" class="clist-down" ng-click="down()">Down</a></div>',
+		link: function (scope, element, attrs) {
+			scope.up = function () {
+				alert("UP!");
+				console.log("Up");
+			}
+
+			scope.down = function () {
+				console.log("Down");
+			}
+            Hamster(element[0]).wheel(function(event, delta, deltaX, deltaY){
+                event.preventDefault();
+              if(delta>0)
+                  scope.down();
+              else
+                  scope.up();
+            });
+		}
+	}
+});
